17091d1f45e0c52743f9e4cd0f4c81419618b783
linamahrouch <lina.mahrouch01@gmail.com>
2023-11-14 19:13:06 +0100

    fix(hero): animation

--- app/components/Hero/index.tsx
+++ app/components/Hero/index.tsx
@@ -26,7 +26,7 @@ export default function Hero() {
                 initial={{ y: "100%", opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 exit={{ y: "-100%", opacity: 0 }}
-                transition={{ duration: 0.4 }}
+                transition={{ duration: 0.3 }}
               >
                 {phrases[phrase % phrases.length]}
               </motion.span>
