e49cf7f21b990a7fc6b720208f800563b43eef1c
LinaMahrouch <lina.mahrouch01@gmail.com>
2024-04-20 02:09:23 +0100

    solve dependency issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+//import { vercelPreset } from '@vercel/remix/vite';
 
 installGlobals();
 
@@ -10,7 +11,7 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
-    remix({
+    remix({      
       ignoredRouteFiles: ["**/*.css"],
     }),tsconfigPaths()
   ],
