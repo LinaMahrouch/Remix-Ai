dd07976e55ddfa108980f1990ccf9c32da77fc14
LinaMahrouch <lina.mahrouch01@gmail.com>
2024-04-27 18:03:47 +0100

    fix navbar issue

--- app/components/Navbar/Index.tsx
+++ app/components/Navbar/Index.tsx
@@ -32,8 +32,7 @@ export default function Navbar() {
       {isMenuOpen && (
         <div className="md:hidden absolute  ">
           <Link to="/" className="block text-white hover:text-gray-300 p-2">Home</Link>
-          {/* <Link to="/features" className="block text-white hover:text-gray-300 p-2">Features</Link> */}
-          <Link to="/login" className="block text-white hover:text-gray-300 p-2">Sign In</Link>
+          <Link to="/signin" className="block text-white hover:text-gray-300 p-2">Sign In</Link>
         </div>
          )}
     </nav>
