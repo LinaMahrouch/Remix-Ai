48923c98cf191acf6c6e23c92562c431bd3035c4
LinaMahrouch <lina.mahrouch01@gmail.com>
2024-04-28 02:39:43 +0100

    remove useless title

--- app/components/Stack/index.tsx
+++ app/components/Stack/index.tsx
@@ -5,7 +5,6 @@ export default function Stack() {
     return (
             <div className="mt-10">
                 <h1 className="text-2xl  font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-                    Built Using:
                 </h1>         
                  <InfiniteMovingCardsDemo />
             </div>
